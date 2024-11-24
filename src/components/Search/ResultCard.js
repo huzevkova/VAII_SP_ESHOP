e407a0968c0865022bb33cdc4af24e6ed0e544c7
huzevkova <huzevkova@stud.uniza.sk>
2024-11-24 18:45:09 +0100

    úprava responzivity

--- src/components/Search/ResultCard.js
+++ src/components/Search/ResultCard.js
@@ -1,32 +1,23 @@
 import {FaShoppingCart} from "react-icons/fa";
 import React from "react";
-import {useNavigate} from "react-router-dom";
 
-
-const ResultCard = () => {
-
-    const navigate = useNavigate();
-
+const ResultCard = ({bookData, goToBookDetail}) => {
     return (
-            <div className="row bg-light p-3 mb-3 border rounded align-items-center relative-position clickable-component" onClick={() => navigate('/book_detail')}>
-                <div className="col-md-3">
+            <div className="row bg-light p-3 mb-3 border rounded align-items-center relative-position clickable-component" onClick={() => goToBookDetail(bookData.id)}>
+                <div className="col-md-2 col-sm-3">
                     <img src="/images/book.jpg" alt="Book Cover" className="img-fluid"/>
                 </div>
-                <div className="col-md-8">
-                    <h5 className="title">Názov knihy</h5>
-                    <p className="text-muted author">Meno autora</p>
-                    <p className="line-clamp description">Krátky popis danej knihy, zobratí
-                        z detailu knihy. V prípade, že je príliš dlhý, tak sa ukáže iba
-                        kúsok a zvyšok sa usekne pridaním trojbodky. Lorem ipsum dolor sit
-                        amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                        ut labore et dolore magna aliqua.</p>
+                <div className="col-md-8 col-sm-7">
+                    <h5 className="title">{bookData.title}</h5>
+                    <p className="text-muted author">{bookData.author}</p>
+                    <p className="line-clamp description">{bookData.description}</p>
                 </div>
-                <div className="col-md-1 cart-price-container">
+                <div className="col-md-2 col-sm-2 cart-price-container text-center">
                     <button className="top-button align-content-center" type="button"><FaShoppingCart/></button>
-                    <h4 className="mt-2 bottom">CENA</h4>
+                    <h4 className="bottom">{bookData.price}</h4>
                 </div>
             </div>
-)
-}
+    )
+};
 
 export default ResultCard;
