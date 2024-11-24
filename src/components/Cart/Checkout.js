25951fa621d03b03c5349331c6c070fc0dc74145
huzevkova <huzevkova@stud.uniza.sk>
2024-11-24 12:20:34 +0100

    Refaktoring komponentov objednávania na Views - rozdelenie logiky a vzhľadu

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import {useNavigate} from "react-router-dom";
-import CheckoutView from "../../views/CheckoutView";
+import CheckoutView from "../../views/Checkout/CheckoutView";
 
 const Checkout = () => {
 
