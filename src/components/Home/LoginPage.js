39ddaaed38ec42e1a61c21c3c417cdfda0a11d0b
huzevkova <huzevkova@stud.uniza.sk>
2024-12-01 00:14:45 +0100

    Pridanie prihlasovacej logiky

--- src/components/Home/LoginPage.js
+++ src/components/Home/LoginPage.js
@@ -1,7 +1,12 @@
 import React, {useState} from 'react';
 import LoginView from "../../views/LoginView";
+import {checkUserCredentials} from "../../api/userApi";
+import {useNavigate} from "react-router-dom";
 
 const LoginPage = () => {
+
+    const navigate = useNavigate();
+
 
     const [loginData, setLoginData] = useState(
         {email: null,
@@ -16,12 +21,23 @@ const LoginPage = () => {
         setLoginState({...loginState, [e.target.name]: 'normal'});
     }
 
-    const handleLogin = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault();
         if (loginData.email == null) {
             setLoginState({...loginState, email: 'wrong'})
         } else if (loginData.password == null) {
             setLoginState({...loginState, password: 'wrong'})
+        } else {
+            try {
+                const response = await checkUserCredentials(loginData.email);
+                if (response.type === 0) {
+                    navigate('/');
+                } else if (response.type === 1) {
+                    navigate('/administration');
+                }
+            } catch (err) {
+                console.error(err);
+            }
         }
     }
 
