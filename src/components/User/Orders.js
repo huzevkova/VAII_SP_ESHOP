1789bb9689d86b12ca3cd00239a237de6ff19a8f
huzevkova <huzevkova@stud.uniza.sk>
2024-12-29 21:22:52 +0100

    úprava vzhľadu histórie objednávok

--- src/components/User/Orders.js
+++ src/components/User/Orders.js
@@ -5,6 +5,8 @@ import MyNavbar from "../General/MyNavbar";
 import Footer from "../General/Footer";
 import Sidebar from "../General/Sidebar";
 import {fetchUserOrders} from "../../api/orderApi";
+import {FaShoppingCart} from "react-icons/fa";
+import {format} from "date-fns";
 
 const Orders = () => {
 
@@ -46,20 +48,16 @@ const Orders = () => {
                         <h2 className="mt-3 text-center text-primary">OBJEDNÁVKY</h2>
                         <div className="container mt-4">
                             {orders.map((order, index) => (
-                                <div className="d-flex justify-content-end" key={index}>
-                                    <>
-                                        <div>
-                                            <div className="col-md-6 col-sm-5">
-                                                <h4 className="title">Objednávka: {order.id_order}</h4>
-                                                <h5 className="text-muted author">Dátum: {order.purchase_date}</h5>
-                                                <h5 className="line-clamp description">Stav: {order.description}</h5>
-                                            </div>
-                                            <div className="col-md-6 col-sm-5 cart-price-container text-center">
-                                                <h4 className="top-button align-content-center">Cena: {order.price}</h4>
-                                                <h4 className="bottom">Počet položiek: {order.number_of_items}</h4>
-                                            </div>
-                                        </div>
-                                    </>
+                                <div className="row bg-light p-3 mb-3 border rounded align-items-center relative-position clickable-component">
+                                    <div className="col-md-8 col-sm-7">
+                                        <h4>Objednávka: {order.id_order}</h4>
+                                        <h5>Dátum: {format(new Date(order.purchase_date), "dd/MM/yyyy HH:mm")}</h5>
+                                        <h5>Stav: {order.description}</h5>
+                                    </div>
+                                    <div className="col-md-4 col-sm-3 cart-price-container text-center">
+                                        <h4 className="align-content-center">Cena: {order.price}</h4>
+                                        <h4 className="bottom">Počet položiek: {order.number_of_items}</h4>
+                                    </div>
                                 </div>
                             ))}
                         </div>
