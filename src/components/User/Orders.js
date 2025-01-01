0f9eddbe31853190696d719398b35641183ca51b
huzevkova <huzevkova@stud.uniza.sk>
2025-01-01 18:04:27 +0100

    pridanie vypísania položiek objednávky

--- src/components/User/Orders.js
+++ src/components/User/Orders.js
@@ -4,13 +4,14 @@ import {useNavigate} from "react-router-dom";
 import MyNavbar from "../General/MyNavbar";
 import Footer from "../General/Footer";
 import Sidebar from "../General/Sidebar";
-import {fetchUserOrders} from "../../api/orderApi";
+import {fetchCartItems, fetchUserOrders} from "../../api/orderApi";
 import {format} from "date-fns";
 
 const Orders = () => {
 
     const navigate = useNavigate();
     const [orders, setOrders] = useState(null);
+    const [orderData, setOrderData] = useState(null);
     const {user} = useAuth();
 
     if (!user) {
@@ -31,8 +32,36 @@ const Orders = () => {
         loadOrders();
     });
 
+    useEffect(() => {
+        const loadOrdersItems = async () => {
+            try {
+                const responses = await Promise.all(
+                    orders.map(order => fetchCartItems(order.id_order))
+                );
+
+                const dataMap = orders.reduce((acc, order, index) => {
+                    acc[order.id_order] = responses[index];
+                    return acc;
+                }, {});
+
+                setOrderData(dataMap);
+            } catch (err) {
+                console.error(err);
+                setOrderData([]);
+            }
+        };
+
+        if (orders != null) {
+            loadOrdersItems();
+        }
+    }, [orders]);
+
     if (orders == null) {
         return;
+    } else {
+        if (orders.length > 0 && orderData == null) {
+            return;
+        }
     }
 
     return (
@@ -47,15 +76,16 @@ const Orders = () => {
                         <h2 className="mt-3 text-center text-primary">OBJEDNÁVKY</h2>
                         <div className="container mt-4">
                             {orders.map((order, index) => (
-                                <div className="row bg-light p-3 mb-3 border rounded align-items-center relative-position clickable-component">
+                                <div className="row bg-light p-3 mb-3 border rounded align-items-center relative-position">
                                     <div className="col-md-8 col-sm-7">
-                                        <h4>Objednávka: {order.id_order}</h4>
+                                        <h4 className="fw-bold">Objednávka: {order.id_order}</h4>
                                         <h5>Dátum: {format(new Date(order.purchase_date), "dd/MM/yyyy HH:mm")}</h5>
                                         <h5>Stav: {order.description}</h5>
                                     </div>
                                     <div className="col-md-4 col-sm-3 cart-price-container text-center">
-                                        <h4 className="align-content-center">Cena: {order.price}</h4>
-                                        <h4 className="bottom">Počet položiek: {order.number_of_items}</h4>
+                                        <h5 className="align-content-center fw-bold">Cena: {order.price}</h5>
+                                        <h5 className="bottom mb-2">Počet položiek: {order.number_of_items}</h5>
+                                        <h5>Položky: {orderData[order.id_order].map(book => book.title).join(", ")}</h5>
                                     </div>
                                 </div>
                             ))}
